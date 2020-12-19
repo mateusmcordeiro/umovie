9022a314fe9a64f1e2e39134f5dcf4d722ca5a31
Mateus Cordeiro <mateusmcordeiro@hotmail.com>
2020-12-19 09:54:28 -0300

    created base design scss and webpack path aliases

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,8 @@ const pathAliasMap = {
   '@shared': path.join(__dirname, './src/shared/'),
   '@components': path.join(__dirname, './src/shared/components/'),
   '@utils': path.join(__dirname, './src/shared/utils/'),
-  '@views': path.join(__dirname, './src/views/')
+  '@views': path.join(__dirname, './src/views/'),
+  '@design': path.join(__dirname, './src/design/'),
 }
 
 module.exports = {
